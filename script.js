385ab5d324dfef7069dd25e8eb5de6b1d19ce155
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-02-10 12:31:04 +0100

    js problems

--- script.js
+++ script.js
@@ -80,3 +80,65 @@ function fib(n){
         return fib(n-1)+fib(n-2)
     }
 }
+
+
+// sum all numbers
+// 4 = 3+2+1
+function sumRange(n){
+    if(n===1){ // base case
+        return 1
+    } else{
+        return n+sumRange(n-1)
+    }
+}
+
+// power function
+function power(x, n){
+    if(n==0){
+        return 1
+    } else{
+        return x*power(x,n-1)
+    }
+}
+
+// contains value in nested object
+
+var nestedObject = {
+    data: {
+        info: {
+            stuff: {
+                thing: {
+                    moreStuff: {
+                        magicNumber: 44,
+                        something: 'foo2'
+                    }
+                }
+            }
+        }
+    }
+}
+
+function contains(object, value){
+    for(let key in object){
+        if(typeof object[key] === "object"){
+            return contains(object[key], value)
+        }
+        if(object[key]== value){
+            return true
+        }
+    }
+    return false
+}
+
+// parse multidimensional array
+// var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+
+let seven = [[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]
+function totalIntegers(array2){
+    if(Array.isArray(array2[array2.length-1])){
+        return totalIntegers(array2.length-1)
+    }
+    array2[array2.length-1].pop()
+}
+
+totalIntegers(seven)
